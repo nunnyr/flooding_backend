97a5c41d163a551a0e6759835cc72117a4c69533
Nunny R <nunnyreyes@gmail.com>
2020-11-30 13:46:09 -0500

    note on association

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password, :avatar, :bio, :city, :state, :zipcode
   has_many :favorites
+  #review this association
 end
